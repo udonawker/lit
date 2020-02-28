33884ed2ada81cd9a0fb2e2e4d3371308cc413c0
udonawker <36081408+udonawker@users.noreply.github.com>
2020-02-29 05:36:11 +0900

    Update peco_find_dir.sh

--- os/linux/tool/peco/peco_find_dir.sh
+++ os/linux/tool/peco/peco_find_dir.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
+#!/bin/bash
 
 function peco_find_dir() {
+    local DIR="."
+
+    while DIR="$(find ${DIR} -maxdepth 1 -type d | sed -e 's;^\./;;' | grep -v "^\.$" | sort | peco)"; do
+        if [ -z "${DIR}" ]; then
+            break;
+        fi
+        echo -n "${DIR} (n/c)"; IFS= read -r -n1 -s char;
+        echo -en "\r"
+        printf "\e[2K" # ANSIエスケープシーケンス チートシート
+        #IFS= read -r -n1 -s char;
+        case ${char} in
+        n)
+            ;;
+        c|$'\0a')
+            break;;
+        *)
+            return
+        esac
+    done
+
+    #DIR=$(echo "${DIR}" | sed -e 's/ /\\ /g' -e 's/(/\\(/g' -e 's/)/\\)/g' -e 's/\./\\./g')
+    echo "${DIR}"
+}
+
+function peco_find_dir_x() {
     local DIR="."
     
     while DIR="$(find ${DIR} -maxdepth 1 -type d | sed -e 's;^\./;;' | grep -v "^\.$" | sort | peco)"; do
@@ -23,5 +49,5 @@ function peco_find_dir() {
     
     echo ${DIR}
 }
-GET_DIR="$(peco_find_dir)"
+GET_DIR="$(peco_find_dir_x)"
 echo "GET_DIR=${GET_DIR}"
